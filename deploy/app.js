1a2bad0d6ccf53301241058085cb30ca58a12503
Hadi karimi <mahdikarimi7513k@gmail.com>
2026-09-18 00:42:27 +0330

    Delete deploy directory

--- deploy/app.js
+++
@@ -1,5 +0,0 @@
-// cPanel / Passenger entry shim.
-// If your host insists on `app.js` as the startup file, point it here.
-// Otherwise you can set the startup file directly to:
-//   server/dist-server/server.cjs
-require("./server/dist-server/server.cjs");
